29cd9d3deace8e13eead9fa7b2e6bedf17500eb9
meenathapila <meenadeva2913@gmail.com>
2024-02-29 14:18:09 +0530

    modified

--- calci.java
+++ calci.java
@@ -2,8 +2,8 @@ interface calci{
 public int getsum(int n1,int n2);
 }
 class calciimpl implements calci{
-    public int getsum(int n1,int n2)
+    public int getsum(int n1,int n2,int n3)
     {
-        return n1+n2;
+        return n1+n2+n3;
     }
 }
